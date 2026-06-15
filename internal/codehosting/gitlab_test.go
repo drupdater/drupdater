771c1d7d8a61dc9a861ae306b39755db722361fd
Christian Fritsch <chr.fritsch@gmx.net>
2026-06-15 21:25:20 +0200

    Apply suggestions from code review
    Co-authored-by: Christian Fritsch <chr.fritsch@gmx.net>

--- internal/codehosting/gitlab_test.go
+++ internal/codehosting/gitlab_test.go
@@ -223,7 +223,7 @@ func TestDownloadAndWriteFile_ReturnsErrorOnNonOKStatus(t *testing.T) {
 }
 
 func TestDownloadAndWriteFile_ReturnsErrorOnWriteFileFailure(t *testing.T) {
-	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte("{}"))
 	}))
